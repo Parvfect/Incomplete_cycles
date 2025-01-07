019a86176c0620cddda568dee29327e4458cc0e1
Parvfect <parvagrw02@gmail.com>
2025-01-07 11:03:23 +0530

    Adding job script

--- v2/script.sh
+++ v2/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#PBS -lwalltime=10:00:00
+#PBS -lselect=1:ncpus=1:mem=100gb
 
-
-# 1. Download muscle and store its location as a variable
-
-# 2. Clone Incomplete cycles
-
-# 3. Run synthesis_pipeline whilst passing the muscle location as an argument 
+# 2. Clone Incomplete cycles and run it
+git clone https://github.com/Parvfect/Incomplete_cycles.git
+cd Incomplete_cycles
+cd v2
+pip install -r requirements.txt
+python synthesis_pipeline.py
