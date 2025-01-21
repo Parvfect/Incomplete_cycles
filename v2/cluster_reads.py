cbdd19762e8fdefcc353e4031b6adf97eb99f607
Parvfect <parvagrw02@gmail.com>
2025-01-21 14:37:07 +0000

    Clustering tested on Badread data

--- v2/cluster_reads.py
+++ v2/cluster_reads.py
@@ -5,6 +5,9 @@ import argparse
 from datetime import datetime
 import os
 import json
+import random
+import uuid
+import time
 
 # Parse input filename for reads and original strands. Can filter later. Return recoveries and candidates
 
@@ -13,8 +16,12 @@ parser = argparse.ArgumentParser(
                     epilog='Use parser to set some parameters for clustering')
 
 parser.add_argument('--reads_filepath', type=str, help="Path to reads.fasta")
-parser.add_argument('--info_filepath', type=str, help="Path to original_strands.txt. Also the output filepath.")
+parser.add_argument('--info_filepath', type=str, help="Path to original_strands.txt. Also the default output filepath.")
+parser.add_argument('--output_filepath', type=str, help="Output filepath.")
+parser.add_argument('--sampling_rate', type=float, help="Sampling rate - defaults to 1.0")
+parser.add_argument('--badread_data', action='store_true', help="Badread data flag")
 
+parser.set_defaults(reads_filepath=None, info_filepath=None, output_filepath=None, sample=1.0)
 
 def get_run_information_from_files(info_filepath):
     """Gets the original strands from generated file"""
@@ -27,29 +34,28 @@ def get_run_information_from_files(info_filepath):
 
     return original_strand_ids, coupling_rates, capping_flags, original_strands
 
-def extract_reads_from_fastq(reads_filepath, reverse_oriented=True):
+def extract_reads_from_fastq(reads_filepath, badread_data_flag=False, reverse_oriented=True, sampling_rate=1.0):
 
     # TODO: Add filter option
     # TODO: Use synthesized padded dict to generate some run statistics
     sequenced_strands = postprocess_badread_sequencing_data(fastq_filepath=reads_filepath,
+                                                            badread_data_flag=badread_data_flag,
                                                              reverse_oriented=reverse_oriented)
-    return sequenced_strands
+    return random.sample(sequenced_strands, int(len(sequenced_strands) * sampling_rate))
 
-def cluster_reads(sequenced_strands, original_strands=None):
+def cluster_reads(sequenced_strands, original_strands=None, sampling_rate=1.0):
     """Conducts aligned clustering for a given read bunch and compared with the original strands"""
 
     if original_strands:
         recoveries = conduct_align_clustering(
             original_strand=original_strands,
             trimmed_seqs=sequenced_strands,
-            display=False,
             multiple=True
         )
     else:
         recoveries = conduct_align_clustering(
             original_strand=original_strands,
             trimmed_seqs=sequenced_strands,
-            display=False,
             multiple=True
         )
     return recoveries
@@ -57,17 +63,23 @@ def cluster_reads(sequenced_strands, original_strands=None):
 def create_clustering_report_file(
         recoveries, output_filepath, original_strands=None, coupling_rates=None, capping_flags=None):
 
+    uid = str(uuid.uuid4())
+
+    os.mkdir() # Timestamp make results directory
+
     # Creating results summary if original strands are provided
     if original_strands is not None:
         df = post_process_results(recoveries_strands=list(
             recoveries['recoveries'].values()),
             capping_flags=capping_flags, coupling_rates=coupling_rates)
-        df.to_csv(os.path.join(output_filepath, "results_summary.csv"))
+        df.to_csv(os.path.join(output_filepath, f"results_summary_{uid}.csv"))
 
     # Writing the recoveries object
-    out_file = open(os.path.join(output_filepath, "recoveries.json"), "w")
+    out_file = open(os.path.join(output_filepath, f"recoveries_{uid}.json"), "w")
     json.dump(recoveries, out_file, indent = 6)
     out_file.close()
+
+    # Info file of the run
     
 
 args = parser.parse_args()
@@ -75,17 +87,32 @@ args = parser.parse_args()
 if __name__ == '__main__':
     reads_filepath = args.reads_filepath
     info_filepath = args.info_filepath
+    sampling_rate = float(args.sampling_rate)
+    badread_data_flag = args.badread_data
+    output_filepath = args.output_filepath
+
+    if output_filepath is None:
+        output_filepath = info_filepath
+
     original_strands, experiment_description = None, None
+
+    if args.reads_filepath is None:
+        print("No reads filepath provided!")
+        exit()
 
     # If no original strands, just output the recoveries
     if info_filepath:
         original_strand_ids, coupling_rates, capping_flags, original_strands = get_run_information_from_files(
             info_filepath=info_filepath)
 
-    sequenced_strands = extract_reads_from_fastq(reads_filepath=reads_filepath)
+    sequenced_strands = extract_reads_from_fastq(reads_filepath=reads_filepath, badread_data_flag=badread_data_flag, sampling_rate=sampling_rate)
+    
+    print(f"Number of strands in the pool = {len(sequenced_strands)}")
 
-    recoveries = cluster_reads(sequenced_strands=sequenced_strands, original_strands=original_strands)
+    recoveries = cluster_reads(sequenced_strands=sequenced_strands, original_strands=original_strands, sampling_rate=sampling_rate)
 
-    create_clustering_report_file(recoveries=recoveries, output_filepath=info_filepath,
+    print(recoveries)
+
+    create_clustering_report_file(recoveries=recoveries, output_filepath=output_filepath,
                                   original_strands=original_strands, coupling_rates=coupling_rates,
                                   capping_flags=capping_flags)
